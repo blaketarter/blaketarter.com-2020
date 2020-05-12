a7af73462cdc69a7a308d0caa03cfcff1b8e0a53
Blake Tarter <blakertarter@gmail.com>
2020-05-12 12:22:54 -0500

    Upgrade deps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,9 @@ export default function App() {
       <Head>
         <meta
           httpEquiv="Content-Security-Policy"
-          content="default-src 'none'; script-src 'self' 'unsafe-inline'; connect-src 'self'; img-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self'; manifest-src 'self';"
+          content={`default-src 'none'; script-src 'self' 'unsafe-inline'${
+            process.env.NODE_ENV === "development" ? " 'unsafe-eval'" : ""
+          }; connect-src 'self'; img-src 'self'; style-src 'self' 'unsafe-inline'; font-src 'self'; manifest-src 'self';`}
         />
         <link
           rel="apple-touch-icon"
