04d8f6f77bddb144db67f83faca1f979abcec115
Blake Tarter <blakertarter@gmail.com>
2020-09-12 16:37:35 -0500

    Add unit tests, just smoke tests for now (#10)

---
+++ config/jest/setupTests.js
@@ -0,0 +1,20 @@
+// optional: configure or set up a testing framework before each test
+// if you delete this file, remove `setupFilesAfterEnv` from `jest.config.js`
+
+// used for __tests__/testing-library.js
+// learn more: https://github.com/testing-library/jest-dom
+import "@testing-library/jest-dom/extend-expect"
+
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+})
